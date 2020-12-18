0a77f365d9e45fdbd60a1d24acc96de4af22430e
Emiliano Pamont <emipmttt@gmail.com>
2020-12-18 12:32:58 -0600

    update/ adding vuetify, router strucutre, views structure

---
+++ src/views/Auth/Auth.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-text-field label="Filled" placeholder="Placeholder"></v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
