a78ca598b374f8883ac8a164853356c0f9f98092
Max23Esau <esa-max23@live.com>
2020-12-18 23:01:51 -0600

    update /add VacantItem

--- src/views/Home/components/Vacant/components/VacantItem.vue
+++ src/views/Home/components/Vacant/components/VacantItem.vue
@@ -1,1 +1,44 @@
-<template></template>
+<template>
+  <article class="VacantItem secondary--bg">
+      <div class="items">
+        <p class="job">Frontend Developer</p>
+        <p class="time">Full Time</p>
+        <p class="company">Globant</p>
+        <p class="modality">Remoto</p>
+        <p class="country">Colombia</p>
+      </div>
+      <div class="buttons">
+        <button>Practicar</button>
+        <button>Aplicar</button>
+      </div>
+    </article>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+  .VacantItem {
+    background: gold;
+    width: 500px;
+    border-radius: 5px;
+    padding: 1.2em;
+  }
+  .items {
+    display: flex;
+    justify-content: space-around;
+    & p{
+      font-size: 1.2em;
+    }
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: flex-end;
+    & .button {
+      margin: 0 10px;
+    }
+  }
+</style>
