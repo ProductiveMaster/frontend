865cdea2480e6d250ff4711b7ace0d403e5bb04f
Max23Esau <esa-max23@live.com>
2020-12-19 01:30:21 -0600

    update /modify VacantItem

--- src/views/Home/components/Vacant/Vacant.vue
+++ src/views/Home/components/Vacant/Vacant.vue
@@ -4,16 +4,21 @@
       <CreateVacant />
     </Modal>
     <div @click="showCreateVacant = true" class="fab">+</div>
+    <div>
+      <VacanteItem />
+    </div>
   </div>
 </template>
 
 <script>
 import CreateVacant from "./components/CreateVacant";
 import Modal from "@/components/Modal";
+import VacanteItem from "./components/VacantItem"
 export default {
   components: {
     CreateVacant,
     Modal,
+    VacanteItem,
   },
   data() {
     return {
--- src/views/Home/components/Vacant/components/VacantItem.vue
+++ src/views/Home/components/Vacant/components/VacantItem.vue
@@ -1,17 +1,24 @@
 <template>
-  <article class="VacantItem secondary--bg">
-      <div class="items">
-        <p class="job">Frontend Developer</p>
-        <p class="time">Full Time</p>
-        <p class="company">Globant</p>
-        <p class="modality">Remoto</p>
-        <p class="country">Colombia</p>
-      </div>
+  <div>
+  <table class="VacantItem tertiary--bg">
+      <tbody class="items">
+          <td>Frontend Developer</td>
+          <td>Full Time</td>
+          <td>Globant</td>
+          <td>Remoto</td>
+          <td>Colombia</td>
+        <!-- <span class="job">Frontend Developer</span>
+        <span class="time">Full Time</span>
+        <span class="company">Globant</span>
+        <span class="modality">Remoto</span>
+        <span class="country">Colombia</span> -->
+      </tbody>
       <div class="buttons">
-        <button>Practicar</button>
-        <button>Aplicar</button>
+        <button class="button">Practicar</button>
+        <button class="button">Aplicar</button>
       </div>
-    </article>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -21,15 +28,19 @@ export default {
 
 <style lang="scss">
   .VacantItem {
-    background: gold;
-    width: 500px;
+    width: 100%;
     border-radius: 5px;
     padding: 1.2em;
   }
   .items {
     display: flex;
     justify-content: space-around;
-    & p{
+    margin-bottom: 1em;
+    & span{
+      font-size: 1.2em;
+    }
+    & td {
+      padding: 10px;
       font-size: 1.2em;
     }
   }
