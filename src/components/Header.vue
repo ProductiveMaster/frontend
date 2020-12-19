7434c2c6d5c587f861062c9d0eaf5a07a43fde16
Max23Esau <esa-max23@live.com>
2020-12-18 18:56:27 -0600

    update/ adding header and sidebar

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="secondary--bg header">
+    <div class="logo">
+      <img src="../assets/logotipo.png" alt="Logotipo">
+    </div>
+    <!--Boton para coach-->
+    <div class="userName">
+      <a href="#" class="light--text">Salir</a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+  .header{
+    background: #1c2d56;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100px;
+    width: 100%;
+    padding: 1em;
+    box-shadow: 0px 0px 10px 1px #000;
+    .logo{
+      img{
+        border-style: none;
+        max-height: 80px;
+      }
+    }
+    .userName {
+      a {
+        text-decoration: none;
+        font-size: 1.5em;
+      }
+    }
+  }
+</style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import { Header } from "./Header";
+import { Sidebar } from "./Sidebar";
+export default {};
+component:{
+  Header,
+  Sidebar
+}
+</script>
+
+<style lang="scss">
+  
+</style>
